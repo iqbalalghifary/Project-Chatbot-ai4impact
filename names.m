b3460c19759ec6087eabf6ab1293b8d50121c70d
Angelia Christy <66985030+angelchrst@users.noreply.github.com>
2022-01-29 19:54:37 +0700

    Create names.m

---
+++ names.m
@@ -0,0 +1,17 @@
+\
+\ Copyright © 2021 angelchrst all rights reserved.
+\
+\ @author: angelchrst
+\ @date: 26 Jan 2022
+\
+\ daftar nama.
+\
+@: names
+names: angel fhilip ikbal nada rama
+
+Q: $names
+A: Halo, $names ! Layanan apa yang ingin kamu pilih ?
+--
+
+
+
